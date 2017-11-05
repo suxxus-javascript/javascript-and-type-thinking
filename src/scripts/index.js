efaa4be5291c80460a77e3649300dfbc7e653264
suxxus <suxxus@outlook.com>
2017-11-05 20:11:33 +0100

    Created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,18 @@
-module.exports = () => true;
+const fs = require('fs');
+const { Future } = require('ramda-fantasy');
+
+const Result = (isError, data) => ({
+  isError, data,
+});
+// public factories
+Result.Success = val => Result(false, val);
+Result.Error = err => Result(true, err);
+
+const readFile = (file = '') => Future((reject, resolve) => {
+  fs.readFile(file, 'utf8', (err, data) =>
+    (err ?
+      reject(Result.Error(err)) :
+      resolve(Result.Success(data))));
+});
+
+module.exports = readFile;
--- src/tests/scripts/index.js
+++ src/tests/scripts/index.js
@@ -1,9 +1,49 @@
-const test = require('tape');
+const path = require('path');
+const test = require('blue-tape');
 const main = require('scripts');
 
-test('index', (t) => {
-  const actual = main();
-  const expect = true;
-  t.ok(actual, expect);
+test('resolved', (t) => {
+  const file = (path.resolve(process.cwd(), 'src/fake-data.txt'));
+  main(file)
+    .fork(
+      () => {},
+      ({ isError = false }) => {
+        const actual = isError;
+        const expect = false;
+        t.equal(actual, expect);
+      },
+
+    );
+  t.end();
+});
+
+test('rejected', (t) => {
+  const file = (path.resolve(process.cwd(), 'src/-fake-data.txt'));
+  main(file)
+    .fork(
+      ({ isError = false }) => {
+        const actual = isError;
+        const expect = true;
+        t.equal(actual, expect);
+      },
+      () => {},
+    );
+  t.end();
+});
+
+
+test('length', (t) => {
+  const file = (path.resolve(process.cwd(), 'src/fake-data.txt'));
+  main(file)
+    .map(obj => obj.data)
+    .map(str => str.length)
+    .fork(
+      () => {},
+      (n = 0) => {
+        const actual = n;
+        const expect = 13;
+        t.equal(actual, expect);
+      },
+    );
   t.end();
 });
